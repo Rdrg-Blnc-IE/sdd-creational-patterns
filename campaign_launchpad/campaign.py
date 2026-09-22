2328ed8fe1dd86c7939417ffb36359f9e2690fa6
Rodrigo Blanco <rblanco.ieu2024@student.ie.edu>
2026-09-22 12:19:05 +0200

    Exercise 1, Campaign builder

--- campaign_launchpad/campaign.py
+++ campaign_launchpad/campaign.py
@@ -1,7 +1,7 @@
 
 from dataclasses import dataclass
 from datetime import date
-from typing import Optional, Dict, Any, List
+from typing import Optional, Dict, Any, List, Type
 
 
 @dataclass(frozen=True)
@@ -18,37 +18,78 @@ class Campaign:
 
 class CampaignBuilder:
     def __init__(self):
-      # TODO
-      pass
+        self.name = None
+        self.channel = None
+        self.daily_budget = None
+        self.start_date = None
+        self.end_date = None
+        self.target_audience = None
+        self.creatives = None
+        self.tracking = None
 
     def with_name(self, name: str):
-      # TODO
-      pass
+      self.name = name
+      return self
 
     def with_channel(self, channel: str):
-      # TODO
-      pass
+      self.channel = channel
+      return self
 
     def with_budget(self, daily_budget: float):
-      # TODO
-      pass
+      self.daily_budget = daily_budget
+      return self
 
     def with_dates(self, start_date, end_date=None):
-      # TODO
-      pass
+      self.start_date = start_date
+      self.end_date = end_date
+      return self
 
     def with_audience(self, **kwargs):
-      # TODO
-      pass
+      self.target_audience = {
+          **kwargs
+      }
+      return self
 
     def add_creative(self, headline: str, image_url: str):
-      # TODO
-      pass
+      self.creatives = [{
+          'headline': headline,
+          'image_url': image_url
+      }]
+      return self
 
     def with_tracking(self, **kwargs):
-      # TODO
-      pass
+        self.tracking = {
+            **kwargs
+        }
+        return self
 
     def build(self) -> Campaign:
+      if not self.name:
+        raise ValueError('Add name as string')
+
+      if not self.channel:
+        raise ValueError('Add channel as string')
+
+      if self.daily_budget < 0:
+        raise ValueError('Budget must be positive')
+
+      if self.end_date and self.start_date > self.end_date:
+        raise ValueError('start date must be before end date')
+
+      if not self.creatives:
+          raise ValueError('Add creative as string')
+
+      if not self.creatives[0]['headline'] and not self.creatives[0]['image_url']:
+          raise ValueError
+
       # TODO: Validations and return Campaign instance
-      pass
+      return Campaign(
+          self.name,
+          self.channel,
+          self.daily_budget,
+          self.start_date,
+          self.end_date,
+          self.target_audience,
+          self.creatives,
+          self.tracking
+      )
